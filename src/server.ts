72f8e249bd26003bf2fed8bcd5fab0ff422bdc99
devAndre-Isaac <andreisaac344@gmail.com>
2022-01-22 19:31:05 -0300

    feat: :sparkles: add communication with server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,8 @@
-console.log('Dev!')
+import * as dotenv from "dotenv";
+const { Client, Intents } = require('discord.js');
+
+dotenv.config();
+
+const bot = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+bot.login(process.env.DISCORD_TOKEN)
