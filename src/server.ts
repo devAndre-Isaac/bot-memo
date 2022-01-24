40a5515afa656c461465ef48b5f5a5df609d6eb1
devAndre-Isaac <andreisaac344@gmail.com>
2022-01-24 19:36:04 -0300

    refactor: :recycle: remove server code

--- src/server.ts
+++
@@ -1,8 +0,0 @@
-import * as dotenv from "dotenv";
-const { Client, Intents } = require('discord.js');
-
-dotenv.config();
-
-const bot = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
-
-bot.login(process.env.DISCORD_TOKEN)
